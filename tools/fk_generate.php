7e83066a29ed43d2fd0a4cc7137505702f983f2d
François Legastelois <flegastelois@teclib.com>
2018-06-22 18:38:52 +0200

    modif script for help to create relation constant array

--- tools/fk_generate.php
+++ tools/fk_generate.php
@@ -30,8 +30,107 @@
  * ---------------------------------------------------------------------
  */
 
-include ('../inc/includes.php');
+include (__DIR__ . '/../inc/includes.php');
 
+$DB->disableTableCaching();
+
+$RELATION = [];
+
+$tables_iterator = $DB->listTables();
+while ($table = $tables_iterator->next()) {
+  $RELATION[$table['TABLE_NAME']] = [];
+}
+
+$tables_iterator = $DB->listTables();
+while ($table = $tables_iterator->next()) {
+   $fields = $DB->list_fields($table['TABLE_NAME']);
+   foreach ($fields as $field) {
+
+      $fk_table = getTableNameForForeignKeyField($field['Field']);
+
+      if ($fk_table !== "" && array_key_exists($fk_table, $RELATION)) {
+
+         if (array_key_exists($table['TABLE_NAME'], $RELATION[$fk_table])) {
+            if (is_array($RELATION[$fk_table][$table['TABLE_NAME']])) {
+               array_push($RELATION[$fk_table][$table['TABLE_NAME']], $field['Field']);
+            } else {
+               $RELATION[$fk_table][$table['TABLE_NAME']] = [
+                  $RELATION[$fk_table][$table['TABLE_NAME']],
+                  $field['Field']
+               ];
+            }
+         } else {
+            $RELATION[$fk_table][$table['TABLE_NAME']] = $field['Field'];
+         }
+      }
+   }
+}
+
+$nbTables = count($RELATION);
+$i = 1;
+
+echo "[\n";
+foreach($RELATION as $table_name => $relations) {
+  echo "'".$table_name."'\n";
+  $nbRelations = count($relations);
+  $j = 1;
+  if ($nbRelations > 0) {
+    echo "   => [";
+
+   $relations_keys = array_keys($relations);
+   $longest = max(array_map('strlen', $relations_keys));
+
+    foreach($relations as $relation_name => $field) {
+
+      if (is_array($field)) {
+         foreach ($field as $key => $value) {
+            $field[$key] = "'".$value."'";
+         }
+         $fieldEcho = "[";
+         $fieldEcho.= implode(', ', $field);
+         $fieldEcho.= "]";
+      } else {
+         $fieldEcho = "'".$field."'";
+      }
+
+      if ($j != 1) {
+        echo "\n       '".$relation_name."'";
+        for ($k = 0; $k < ($longest-strlen($relation_name)); $k++) {
+         echo " ";
+        }
+        echo "=> ".$fieldEcho."";
+      } else {
+        echo "'".$relation_name."'";
+        for ($k = 0; $k < ($longest-strlen($relation_name)); $k++) {
+         echo " ";
+        }
+        echo "=> ".$fieldEcho."";
+      }
+      if ($j == $nbRelations) {
+        echo "]";
+      } else {
+        echo ",";
+      }
+      $j++;
+    }
+  } else {
+    echo "   => []";
+  }
+
+  if ($i == $nbTables) {
+    echo "\n";
+  } else {
+    echo ",\n";
+  }
+  $i++;
+}
+echo "]\n";
+
+function mysort($a,$b){
+    return strlen($b)-strlen($a);
+}
+
+/*
 $DB->query("SET FOREIGN_KEY_CHECKS = '0';");
 $result = $DB->list_tables();
 $numtab = 0;
@@ -87,4 +186,4 @@ foreach ($query as $table => $constraints) {
 }
 
 $DB->query("SET FOREIGN_KEY_CHECKS = 1;");
-
+*/
